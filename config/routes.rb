5846d2fe3d8e5c905af1755f15daef0b1da33773
Rafael Teodoro <rafaelteodoro@rocketmail.com>
2023-08-24 17:39:27 -0400

    fixed banner and logo

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  root 'lists#index'
+  resources :lists, only: %i[index show new create] do
+    resources :bookmarks, only: %i[new create]
+  end
 
-  # Defines the root path route ("/")
-  # root "articles#index"
-  resources :lists, only: %i[index show new create]
+  resources :bookmarks, only: :destroy
 end
